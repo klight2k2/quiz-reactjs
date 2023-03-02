b467a656f85eac64432407fd0f89467f2d8a2ecd
klight <phamminhquang2k2@gmail.com>
2023-03-02 14:52:00 +0700

    up quiz

---
+++ src/Quiz/Quiz.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import './quiz.scss'
+export default function Quiz({ quiz, handleChangeQuiz }) {
+    const [checkedState, setCheckedState] = useState(new Array(quiz.ans.length).fill(false))
+    const [correct, setCorrect] = useState(new Array(quiz.ans.length).fill(0))
+    const handleOnChange = (position) => {
+        const updatedCheckedState = checkedState.map((item, index) =>
+            index === position ? !item : item
+        );
+        setCheckedState(updatedCheckedState);
+    }
+    const handleSubmit = () => {
+        const newCorrect = checkedState.map((item, idx) => {
+
+            if (quiz.key.includes(quiz.ans[idx])) {
+                return 1;
+            }
+            if (item && !quiz.key.includes(quiz.ans[idx])) {
+                return -1;
+            }
+            return 0;
+        })
+        setCorrect(newCorrect)
+    }
+    useEffect(() => {
+        setCorrect(new Array(quiz.ans.length).fill(0));
+        setCheckedState(new Array(quiz.ans.length).fill(false));
+    }, [quiz])
+    const handleNext = () => {
+        handleChangeQuiz()
+    }
+    return (
+        <div className='quiz '>
+            <h2 className='title' >{quiz.question}</h2>
+            <div className='list '>
+                {quiz.ans.map((item, id) => {
+                    return <div className={correct[id] === 1 ? "correct " : correct[id] === -1 ? "wrong " : ""} key={id}>
+                        <label className='container' htmlFor={id}> {item}
+                            <input type="checkbox" id={id} name={item} value={item} onChange={() => handleOnChange(id)} checked={checkedState[id]} />
+                            <span class="checkmark"></span>
+                        </label>
+                    </div>
+                })}
+            </div >
+
+            {
+                correct.every(item => item === 0) ? <button className='button' onClick={handleSubmit}>Nộp</button> : <button className='button' onClick={handleNext}>Next</button>
+            }
+        </div>
+    )
+}
